88806bcf116cf8c2c95858103222b26cbb84817d
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-21 16:57:29 +0200

    Milestone 2 completed - added v-ifs and elses check for AppCard to display the name whether it is a movie or a tv series, added comments for better understanding

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,13 +1,22 @@
 <template>
     <div id="app-card">
-        <h1>{{ card.title }}</h1>
-        <h2>{{ card.original_title }}</h2>
+        <!-- Property "title" for Movies, "name" for TV Series -->
+        <h1 v-if="card.title">{{ card.title }}</h1>
+        <h1 v-else>{{ card.name }}</h1>
+
+        <!-- Property "original_title" for Movies, "original_name" for TV Series -->
+        <h2 v-if="card.original_title">{{ card.original_title }}</h2>
+        <h2 v-else>{{ card.original_name }}</h2>
+
+        <!-- Flags with correction if missing or value slightly different -->
         <span v-if="(card.original_language === 'en')" class="fi fi-gb"></span>
         <span v-else-if="(card.original_language === 'ko')" class="fi fi-kr"></span>
         <span v-else-if="(card.original_language === 'ja')" class="fi fi-jp"></span>
         <span v-else-if="(card.original_language === 'cs')" class="fi fi-cz"></span>
         <img v-else-if="(card.original_language === 'xx')" src="../../public/error.png">
         <span v-else :class="'fi fi-' + card.original_language"></span>
+
+        <!-- Rating -->
         <h3>{{ card.vote_average }}</h3>
     </div>
 </template>
