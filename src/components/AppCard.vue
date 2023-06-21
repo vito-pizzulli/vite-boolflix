324af635957f0e153547136f22305a562bacf5ba
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-21 16:14:57 +0200

    imported flag-icons library, added flags replacing language, added v-ifs and else to make every flag correcly appear

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -2,12 +2,18 @@
     <div id="app-card">
         <h1>{{ card.title }}</h1>
         <h2>{{ card.original_title }}</h2>
-        <h3>{{ card.original_language }}</h3>
-        <h4>{{ card.vote_average }}</h4>
+        <span v-if="(card.original_language === 'en')" class="fi fi-gb"></span>
+        <span v-else-if="(card.original_language === 'ko')" class="fi fi-kr"></span>
+        <span v-else-if="(card.original_language === 'ja')" class="fi fi-jp"></span>
+        <span v-else-if="(card.original_language === 'cs')" class="fi fi-cz"></span>
+        <img v-else-if="(card.original_language === 'xx')" src="../../public/error.png">
+        <span v-else :class="'fi fi-' + card.original_language"></span>
+        <h3>{{ card.vote_average }}</h3>
     </div>
 </template>
 
 <script>
+import "/node_modules/flag-icons/css/flag-icons.min.css";
 export default {
     name: 'AppCard',
 
@@ -24,5 +30,9 @@ export default {
     #app-card {
         padding: 1rem;
         border: 1px solid black;
+
+        img {
+            width: 25px;
+        }
     }
 </style>
