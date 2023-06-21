7146a30e9f64b93648a407674323ac6b047288df
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-21 14:18:21 +0200

    added getMovieListByQuery func that does an API call based on query inserted in Header searchbar, func is called by an event emitted from the Header

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,41 @@
 <template>
-  <div>
-    <AppHeader />
+    <AppHeader 
+      @startSearch="getMovieListByQuery()"
+    />
     <AppMain />
-  </div>
 </template>
 
 <script>
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
+import { store } from './store';
+import axios from 'axios';
 export default {
   name: 'App',
 
   components: {
     AppHeader,
     AppMain
-  }
+  },
+
+  data() {
+        return {
+            store,
+        }
+    },
+
+    methods: {
+        getMovieListByQuery() {
+            axios.get('https://api.themoviedb.org/3/search/movie?api_key=67aa46a4e4cc94fee02d3448ee99b726&query=' + store.searchQuery)
+                    .then(function (response) {
+                        console.log(response.data);
+                        store.apiCall = response.data;
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    })
+        }
+    }
 }
 </script>
 
