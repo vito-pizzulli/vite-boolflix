1b9dd3dbd857eb9a85467c40698855c4d0d3bad6
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-21 14:27:06 +0200

    imported AppCard in AppContent, add v-for that generate one for every element of the api call array, passing needed info via props

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -1,12 +1,30 @@
 <template>
     <div id="app-content">
         <h1>I'm the App Content</h1>
+        <AppCard v-for="card in store.apiCall"
+            :title="store.apiCall.title"
+            :originalTitle="store.apiCall.originalTitle"
+            :language="store.apiCall.language"
+            :rating="store.apiCall.rating"
+        />
     </div>
 </template>
 
 <script>
+import AppCard from './AppCard.vue';
+import { store } from '../store';
 export default {
-    name:'App Content'
+    name:'App Content',
+
+    components: {
+        AppCard
+    },
+
+    data() {
+        return {
+            store
+        }
+    }
 }
 </script>
 
