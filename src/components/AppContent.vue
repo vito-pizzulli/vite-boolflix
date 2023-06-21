6c552b611037043125475e7a97b2ba838508c419
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-21 12:40:57 +0200

    created AppContent component where the search results will be displayed

---
+++ src/components/AppContent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="app-content">
+        <h1>I'm the App Content</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'App Content'
+}
+</script>
+
+<style lang="scss" scoped>
+    @use '../styles/partials/variables' as *;
+    @use '../styles/partials/mixins' as *;
+
+    #app-content {
+        padding: 1rem;
+        border: 1px solid black;
+    }
+</style>
