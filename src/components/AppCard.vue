2347922cbf70d31488358bf28aa46b1acee61928
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-21 14:26:09 +0200

    created AppCard.vue component

---
+++ src/components/AppCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="app-card">
+        <h1>I'm a Card</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppCard'
+}
+</script>
+
+<style lang="scss" scoped>
+    @use '../styles/partials/variables' as *;
+    @use '../styles/partials/mixins' as *;
+
+    #app-card {
+        padding: 1rem;
+        border: 1px solid black;
+    }
+</style>
