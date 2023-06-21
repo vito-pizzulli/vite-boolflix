87ea6dc25ba93631c1e450123e1b53a5faef9804
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-21 17:24:13 +0200

    implemented array sorted by elements popularity after the movies and tv series array have been merged

--- src/App.vue
+++ src/App.vue
@@ -22,29 +22,36 @@ export default {
         return {
             store,
         }
-    },
+  },
 
-    methods: {
-        getContentListByQuery(searchQuery) {
-            axios.get('https://api.themoviedb.org/3/search/movie?api_key=67aa46a4e4cc94fee02d3448ee99b726&query=' + searchQuery)
-                    .then(function (movieResponse) {
-                        console.log(movieResponse.data.results);
-                        store.apiCall = movieResponse.data.results;
-                    })
-                    .catch(function (error) {
-                        console.log(error);
-                    })
+  methods: {
+      getContentListByQuery(searchQuery) {
+          axios.get('https://api.themoviedb.org/3/search/movie?api_key=67aa46a4e4cc94fee02d3448ee99b726&query=' + searchQuery)
+                  .then(function (movieResponse) {
+                      console.log(movieResponse.data.results);
+                      store.apiCall = movieResponse.data.results;
+                  })
+                  .catch(function (error) {
+                      console.log(error);
+                  })
 
-            axios.get('https://api.themoviedb.org/3/search/tv?api_key=67aa46a4e4cc94fee02d3448ee99b726&query=' + searchQuery)
-                    .then(function (tvResponse) {
-                        console.log(tvResponse.data.results);
-                        store.apiCall = store.apiCall.concat(tvResponse.data.results);
-                    })
-                    .catch(function (error) {
-                        console.log(error);
-                    })
-        }
+          axios.get('https://api.themoviedb.org/3/search/tv?api_key=67aa46a4e4cc94fee02d3448ee99b726&query=' + searchQuery)
+                  .then(function (tvResponse) {
+                      console.log(tvResponse.data.results);
+                      store.apiCall = store.apiCall.concat(tvResponse.data.results);
+                      store.apiCall = store.apiCall.sort((a, b) => b["popularity"] - a["popularity"]);
+                  })
+                  .catch(function (error) {
+                      console.log(error);
+                  })
+      }
+  },
+
+  /* computed: {
+    sortItemsByPopularity() {
+      return store.apiCall.sort((a, b) => b["popularity"] - a["popularity"]);
     }
+  } */
 }
 </script>
 
