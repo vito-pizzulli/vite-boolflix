dbb1aab2cea2134368e5a6086edaf0d9df7c1713
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-22 12:02:52 +0200

    font-awesome correcly installed for Vue

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+library.add(faStar)
 
-createApp(App).mount('#app')
+createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
