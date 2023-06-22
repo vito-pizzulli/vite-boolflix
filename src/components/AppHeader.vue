a1f16af6b8c5ddbe19a2cbd570d0d94f7f695a01
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-22 14:46:38 +0200

    AppHeader styling completed

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,10 +1,23 @@
 <template>
     <header>
-        <div id="app-searchbar">
-            <h1>Boolflix</h1>
-            <input type="text" v-model="store.searchQuery" @keyup.enter="$emit('startSearch')">
-            <button @click="$emit('startSearch')">Search</button>
-    </div>
+        <div class="header-half">
+            <img src="../../public/netflix_logo.png" alt="Netflix Logo">
+            <nav id="header-navbar">
+                <ul>
+                    <li v-for="link in store.navbarLinks"><a href="link.link">{{ link.text }}</a></li>
+                </ul>
+            </nav>
+        </div>
+        <div class="header-half">
+            <div id="app-searchbar">
+                <button @click="inputToggle()" class="search-button"><font-awesome-icon icon="magnifying-glass" /></button>
+                <input v-if="(this.inputVisible === true)" type="text" v-model="store.searchQuery" @keyup.enter="$emit('startSearch')" placeholder="Titolo del film o della serie TV">
+                <button v-if="(this.inputVisible === true)" @click="inputToggle()" class="cross-button"><font-awesome-icon icon="xmark" /></button>
+                <span class="icon"><font-awesome-icon icon="bell" /></span>
+                <img src="../../public/profile_pic.png" alt="Profile Pic">
+                <span class="icon"><font-awesome-icon icon="caret-down" /></span>
+            </div>
+        </div>
     </header>
 </template>
 
@@ -15,7 +28,14 @@ export default {
 
     data() {
         return {
-            store
+            store,
+            inputVisible: false
+        }
+    },
+
+    methods: {
+        inputToggle() {
+            this.inputVisible = !this.inputVisible
         }
     }
 }
@@ -26,7 +46,66 @@ export default {
     @use '../styles/partials/mixins' as *;
 
     header {
-        padding: 1rem;
-        border: 1px solid black;
+        @include flex(row, space-between, center, no-wrap);
+        background-color: black;
+        color: white;
+        padding: 0 3rem;
+
+        div.header-half {
+            @include flex(row, center, center, no-wrap);
+
+            img {
+            width: 150px;
+            }
+
+            nav#header-navbar ul {
+                @include flex(row, start, center, no-wrap);
+                list-style: none;
+
+                li {
+                    margin: 0 1rem;
+
+                    a {
+                        color: white;
+                        text-decoration: none;
+                    }
+                }
+            }
+
+            div#app-searchbar {
+                @include flex(row, center, center, no-wrap);
+
+                img {
+                    width: auto;
+                    margin-left: 1rem;
+                }
+
+                button {
+                    background-color: transparent;
+                    color: white;
+                    border: none;
+                    font-size: 1.2rem;
+                    margin-right: 1rem;
+                }
+
+                button.cross-button {
+                    transform: translateX(-150%);
+                }
+
+                input {
+                    background-color: transparent;
+                    border: 1px solid white;
+                    padding: .5rem;
+                    color: white;
+                    width: 300px;
+                }
+
+                span.icon {
+                    display: inline-block;
+                    margin: 0 1rem;
+                    font-size: 1.2rem;
+                }
+            }
+        }
     }
 </style>
