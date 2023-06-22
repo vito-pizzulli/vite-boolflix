83ea3b96eaed924601cc8687ebf3e61b8fdb88a1
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-22 22:10:58 +0200

    implemented a working structure for the AppMediaPreview component

--- src/components/AppMediaPreview.vue
+++ src/components/AppMediaPreview.vue
@@ -1,6 +1,10 @@
 <template>
-    <div id="app-media-preview">
-        <h1>Sono la preview</h1>
+    <div id="app-media-preview" @click="$emit('closeMediaPreview')">
+        <h1 v-if="card.title">{{ card.title }}</h1>
+        <h1 v-else>{{ card.name }}</h1>
+
+        <img v-if="card.poster_path" :src="'http://image.tmdb.org/t/p/w342/' + card.poster_path" alt="Poster Image" class="poster">
+        <img v-else src="/public/no_image.png" alt="No Poster Found Image" class="poster">
     </div>
 </template>
 
@@ -19,6 +23,14 @@ export default {
     @use '../styles/partials/mixins' as *;
 
     div#app-media-preview {
+        @include flex(column, center, center, no-wrap);
+        background-color: rgba(0, 0, 0, 0.7);
+        z-index: 2;
         color: white;
+        width: 100vw;
+        height: 100vh;
+        position: absolute;
+        top: 0;
+        left: 0;
     }
 </style>
