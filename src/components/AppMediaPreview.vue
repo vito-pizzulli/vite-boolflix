2ef6d93fbeefb8131bda07cff16ba1d6ddc1f61e
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-22 20:37:01 +0200

    created AppMediaPreview component, it will appear on AppCard click and display the media preview

---
+++ src/components/AppMediaPreview.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app-media-preview">
+        <h1>Sono la preview</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppMediaPreview',
+
+    props: {
+        card: Object
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    @use '../styles/partials/variables' as *;
+    @use '../styles/partials/mixins' as *;
+
+    div#app-media-preview {
+        color: white;
+    }
+</style>
