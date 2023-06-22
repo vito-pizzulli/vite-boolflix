b3806d011aa97b213c18996faeb0eaef2a119354
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-22 14:45:46 +0200

    added more icons from font awesome

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faStar } from '@fortawesome/free-solid-svg-icons'
-library.add(faStar)
+import { faStar, faMagnifyingGlass, faBell, faCaretDown, faXmark } from '@fortawesome/free-solid-svg-icons'
+library.add(faStar, faMagnifyingGlass, faBell, faCaretDown, faXmark)
 
 createApp(App)
 .component('font-awesome-icon', FontAwesomeIcon)
