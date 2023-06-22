5ef1aae12d1f07642ad6aa2c7918715a3b1c73f7
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-22 17:18:22 +0200

    v-ifs code shortened

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -4,7 +4,7 @@
             :card="card"
         />
     </div>
-    <h2 v-if="(store.noResults === true)">No results for your search with "{{ store.searchedQuery }}".</h2>
+    <h2 v-if="store.noResults">No results for your search with "{{ store.searchedQuery }}".</h2>
 </template>
 
 <script>
