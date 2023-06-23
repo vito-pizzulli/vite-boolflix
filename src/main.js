868b881a75372375a3ee5f3b4b7808a707e3b06f
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-23 12:32:00 +0200

    added font-awesome circle play icon

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faStar, faMagnifyingGlass, faBell, faCaretDown, faXmark, faPlay } from '@fortawesome/free-solid-svg-icons'
-library.add(faStar, faMagnifyingGlass, faBell, faCaretDown, faXmark, faPlay)
+import { faStar, faMagnifyingGlass, faBell, faCaretDown, faXmark, faPlay, faCirclePlay } from '@fortawesome/free-solid-svg-icons'
+library.add(faStar, faMagnifyingGlass, faBell, faCaretDown, faXmark, faPlay, faCirclePlay)
 
 createApp(App)
 .component('font-awesome-icon', FontAwesomeIcon)
