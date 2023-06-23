275b33214f1e35eb32b95df0004ef9e355aa8b13
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-23 14:23:24 +0200

    added created() with API calls so page is not empty at the start

--- src/components/AppBody.vue
+++ src/components/AppBody.vue
@@ -29,32 +29,53 @@ export default {
     methods: {
         getContentListByQuery() {
             axios.get('https://api.themoviedb.org/3/search/movie?api_key=67aa46a4e4cc94fee02d3448ee99b726&language=it-IT&query=' + store.searchQuery)
-                    .then(function (movieResponse) {
-                        store.searchComplete = false;
-                        store.noResults = false;
-                        store.apiCall = movieResponse.data.results;
-                    })
-                    .catch(function (error) {
-                        console.log(error);
-                    })
+                .then(function (movieResponse) {
+                    store.searchComplete = false;
+                    store.noResults = false;
+                    store.apiCall = movieResponse.data.results;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
 
             axios.get('https://api.themoviedb.org/3/search/tv?api_key=67aa46a4e4cc94fee02d3448ee99b726&language=it-IT&query=' + store.searchQuery)
-                    .then(function (tvResponse) {
-                        store.apiCall = store.apiCall.concat(tvResponse.data.results);
-                        store.apiCall = store.apiCall.sort((a, b) => b["popularity"] - a["popularity"]);
-                        console.log(store.apiCall);
-                        store.searchComplete = true;
+                .then(function (tvResponse) {
+                    store.apiCall = store.apiCall.concat(tvResponse.data.results);
+                    store.apiCall = store.apiCall.sort((a, b) => b["popularity"] - a["popularity"]);
+                    console.log(store.apiCall);
+                    store.searchComplete = true;
 
-                        if (store.apiCall.length === 0) {
-                        store.noResults = true;
-                        }
-                    })
-                    .catch(function (error) {
-                        console.log(error);
-                    })
+                    if (store.apiCall.length === 0) {
+                    store.noResults = true;
+                    }
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
 
-                    store.searchedQuery = store.searchQuery
+                store.searchedQuery = store.searchQuery
         }
+    },
+
+    created() {
+        axios.get('https://api.themoviedb.org/3/search/movie?api_key=67aa46a4e4cc94fee02d3448ee99b726&language=it-IT&query=a')
+            .then(function (movieResponse) {
+                store.apiCall = movieResponse.data.results;
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
+
+        axios.get('https://api.themoviedb.org/3/search/tv?api_key=67aa46a4e4cc94fee02d3448ee99b726&language=it-IT&query=b')
+            .then(function (tvResponse) {
+                store.apiCall = store.apiCall.concat(tvResponse.data.results);
+                store.apiCall = store.apiCall.sort((a, b) => b["popularity"] - a["popularity"]);
+                console.log(store.apiCall);
+                store.searchComplete = true;
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
     }
 }
 </script>
