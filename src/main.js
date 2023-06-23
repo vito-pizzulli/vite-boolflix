3da2709b14c76574ec22119305c06e8f6cea577b
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-23 15:21:30 +0200

    added more font-awesome icons

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faStar, faMagnifyingGlass, faBell, faCaretDown, faXmark, faPlay, faCirclePlay } from '@fortawesome/free-solid-svg-icons'
-library.add(faStar, faMagnifyingGlass, faBell, faCaretDown, faXmark, faPlay, faCirclePlay)
+import { faStar, faMagnifyingGlass, faBell, faCaretDown, faXmark, faPlay, faCirclePlay, faPencil, faUser, faCircleQuestion, faBan } from '@fortawesome/free-solid-svg-icons'
+library.add(faStar, faMagnifyingGlass, faBell, faCaretDown, faXmark, faPlay, faCirclePlay, faPencil, faUser, faCircleQuestion, faBan)
 
 createApp(App)
 .component('font-awesome-icon', FontAwesomeIcon)
