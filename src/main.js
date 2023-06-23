009182df0131eb96b9aceaf91d09f32634015894
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-23 11:53:30 +0200

    added font-awesome play icon

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faStar, faMagnifyingGlass, faBell, faCaretDown, faXmark } from '@fortawesome/free-solid-svg-icons'
-library.add(faStar, faMagnifyingGlass, faBell, faCaretDown, faXmark)
+import { faStar, faMagnifyingGlass, faBell, faCaretDown, faXmark, faPlay } from '@fortawesome/free-solid-svg-icons'
+library.add(faStar, faMagnifyingGlass, faBell, faCaretDown, faXmark, faPlay)
 
 createApp(App)
 .component('font-awesome-icon', FontAwesomeIcon)
